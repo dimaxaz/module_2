aab2a374473ceb84213dbeaaf7f6d4a43c925890
dimaxaz <145430196+dimaxaz@users.noreply.github.com>
2024-09-22 22:29:10 +0300

    task_3 Цикл While

---
+++ modul_2_3.py
@@ -0,0 +1,14 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+
+i = 0
+
+while i < len(my_list):
+    number = my_list[i]
+
+    if number < 0:
+        break
+
+    if number > 0:
+        print(number)
+
+    i += 1
